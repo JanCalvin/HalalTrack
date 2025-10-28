3e998474a931fa1c933dc1a4866fcf6685f7dbb5
JanCalvin <jannn.losik@gmail.com>
2025-10-28 15:49:25 +0700

    'settingan'

--- haltrack/settings.py
+++ haltrack/settings.py
@@ -79,36 +79,19 @@ ASGI_APPLICATION = "haltrack.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=os.getenv("DB_SSL_REQUIRE", "1") == "1",
-        )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME","postgres"),
+        'USER':os.getenv("DB_USER","postgres"),
+        'PASSWORD': os.getenv("DB_PASSWORD",""),
+        'HOST': os.getenv("DB_HOST", "localhost"),
+        'PORT':os.getenv("DB_PORT", "6543"),
+        'OPTIONS': {
+            'sslmode':"require" ,
+        },
     }
-else:
-    # fallback sementara (tidak persisten di Vercel) agar tidak crash
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": "/tmp/db.sqlite3",
-        }
-    }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv("DB_NAME","postgres"),
-#         'USER':os.getenv("DB_USER","postgres"),
-#         'PASSWORD': os.getenv("DB_PASSWORD",""),
-#         'HOST': os.getenv("DB_HOST", "localhost"),
-#         'PORT':os.getenv("DB_PORT", "6543"),
-#         'OPTIONS': {
-#             'sslmode':"require" ,
-#         },
-#     }
-# }
+}
 # WhiteNoise storage (kompres + cache-busting)
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
@@ -145,7 +128,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"        # <— target collectstatic
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
