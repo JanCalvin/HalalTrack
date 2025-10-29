4fc5418aa58d33d6141fe2f6d490ad54d27c4696
JanCalvin <jannn.losik@gmail.com>
2025-10-29 19:57:27 +0700

    bias2

--- haltrack/settings.py
+++ haltrack/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'halal',
+    "whitenoise.runserver_nostatic",
 ]
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 MIDDLEWARE = [
